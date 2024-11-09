0adf9541f20674cea5a157053805dbc9adb79f56
John Christian Vicente <johnc.vicente1@gmail.com>
2024-11-10 00:05:29 +0800

    feat: add products, suppliers, and sales tables

--- app/schema.sql
+++ app/schema.sql
@@ -12,16 +12,31 @@ CREATE TABLE IF NOT EXISTS user_inventory (
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS store_inventory (
+CREATE TABLE IF NOT EXISTS products (
     product_id INTEGER PRIMARY KEY AUTOINCREMENT,
     inventory_id INTEGER NOT NULL,
-    product_name TEXT NOT NULL,
+    product_name TEXT NOT NULL UNIQUE,
     price DECIMAL(10,2) NOT NULL,
     quantity INTEGER NOT NULL DEFAULT 0,
     last_updated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (inventory_id) REFERENCES user_inventory(inventory_id) ON DELETE CASCADE
 );
 
--- Performance kuno nvm
+CREATE TABLE IF NOT EXISTS suppliers(
+    supplier_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    product_id INTEGER NOT NULL,
+    supplier_name TEXT NOT NULL UNIQUE,
+    FOREIGN KEY (product_id) REFERENCES products(product_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS sales(
+    sale_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    product_id INTEGER NOT NULL,
+    sale_count INTEGER NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES products(product_id) ON DELETE CASCADE
+);
+
+-- Indexes
 CREATE INDEX idx_user_inventory_user_id ON user_inventory(user_id);
-CREATE INDEX idx_product_id ON store_inventory(product_id)
+CREATE INDEX idx_product_inventory_id ON products(inventory_id);
+CREATE INDEX idx_supplier_product_id ON suppliers(product_id);
