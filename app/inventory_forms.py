d38a475bece99b59f79db8fe36a66637c16a96ca
John Christian Vicente <johnc.vicente1@gmail.com>
2024-11-10 00:05:14 +0800

    feat: add product form

--- app/inventory_forms.py
+++ app/inventory_forms.py
@@ -1,11 +1,45 @@
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm
+from wtforms import FloatField, IntegerField, StringField, SubmitField
+from wtforms.validators import DataRequired, Length, NumberRange, Optional
 
 
-class ProductForm:
+class ProductForm(FlaskForm):
     product_name = StringField(
-        "Username",
+        "Product Name",
         validators=[
-            DataRequired("This field is required"),
+            DataRequired("Product name is required"),
+            Length(
+                min=3,
+                message="Product name must be 3 characters long",
+            ),
         ],
     )
+    price = FloatField(
+        "Price",
+        default=0,
+        validators=[
+            Optional(),
+            NumberRange(min=0, message="Price must be greater than or equal to 0"),
+        ],
+    )
+    quantity = IntegerField(
+        "Quantity",
+        default=0,
+        validators=[
+            Optional(),
+            NumberRange(min=0, message="Quantity must be greater than or equal to 0"),
+        ],
+    )
+    supplier_name = StringField(
+        "Supplier Name",
+        validators=[
+            DataRequired("Supplier name is required"),
+            Length(
+                min=3,
+                message="Supplier name must be 3 characters long",
+            ),
+        ],
+    )
+    add_submit = SubmitField("Add Product")
+    edit_submit = SubmitField("Save Changes")
+    remove_submit = SubmitField("Remove Product")
