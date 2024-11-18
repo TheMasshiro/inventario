c7876ab88cd3c915800bcb046c5a7fa60a54a124
John Christian Vicente <johnc.vicente1@gmail.com>
2024-11-18 21:20:42 +0800

    refactor: remove duplicate pagination logic and consolidate pagination configuration

--- app/helpers.py
+++ app/helpers.py
@@ -1,3 +1,6 @@
+ITEMS_PER_PAGE = 10
+
+
 def format_currency(value):
     """Format currency to PHP"""
     return "₱{:,.2f}".format(value)
@@ -8,53 +11,3 @@ def error_messages(form):
         if errors:
             return errors[0]
     return ""
-
-
-def paginate_suppliers(page, user_suppliers):
-    """Paginate table values"""
-    ITEMS_PER_PAGE = 10
-
-    total_items = user_suppliers.get_total_suppliers()
-    total_pages = max(1, (total_items + ITEMS_PER_PAGE - 1) // ITEMS_PER_PAGE)
-
-    if total_pages <= 5:
-        start_page = 1
-        end_page = total_pages
-    else:
-        start_page = max(1, page - 2)
-        end_page = min(total_pages, page + 2)
-
-        if start_page == 1:
-            end_page = 5
-        elif end_page == total_pages:
-            start_page = total_pages - 4
-
-    offset = (page - 1) * ITEMS_PER_PAGE
-    products = user_suppliers.get_all_paginated_suppliers(offset, ITEMS_PER_PAGE)
-
-    return [products, start_page, end_page, total_pages]
-
-
-def paginate_inventory(page, user_inventory):
-    """Paginate table values"""
-    ITEMS_PER_PAGE = 10
-
-    total_items = user_inventory.get_total_items()
-    total_pages = max(1, (total_items + ITEMS_PER_PAGE - 1) // ITEMS_PER_PAGE)
-
-    if total_pages <= 5:
-        start_page = 1
-        end_page = total_pages
-    else:
-        start_page = max(1, page - 2)
-        end_page = min(total_pages, page + 2)
-
-        if start_page == 1:
-            end_page = 5
-        elif end_page == total_pages:
-            start_page = total_pages - 4
-
-    offset = (page - 1) * ITEMS_PER_PAGE
-    products = user_inventory.get_all_paginated_items(offset, ITEMS_PER_PAGE)
-
-    return [products, start_page, end_page, total_pages]
